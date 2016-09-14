4c0d38d67c5ee7b866dad1aafd725084c2281daa
Sachin Karia <sachin.karia01@gmail.com>
2016-09-14 16:35:53 +0100

    edited directive and file upload template

--- directive.js
+++ directive.js
@@ -1,69 +1,82 @@
-uploader.directive('uploadFile', ['$timeout','$sce', '$http',function ($timeout, $sce, $http) {
+(function() {
+  'use strict';
 
-  return {
-    restrict: 'E',
-    templateUrl: 'fileUpload.html',
-    replace: true,
-    link: link,
-    scope: {
-      id: "@id",
-      wistiaPassword: "@password"
-    }
-  };
+  angular
+    .module('uploader')
+    .directive('uploadfile', uploadFileDirective);
 
-  function link(scope, element, attrs) {
-    scope.hashId   = '';
-    scope.progress = 0;
-    scope.status   = 'UPLOAD A VIDEO';
-    scope.url      = '';
+  uploadFileDirective.$inject = ['$timeout','$sce', '$http'];
 
-    scope.checkStatus = function() {
-      $http({
-        method: 'GET',
-        url: 'https://api.wistia.com/v1/medias/' + scope.hashId + '.json?api_password=' + scope.wistiaPassword
-      }).then(function (response) {
-        scope.status = response.data.status || '';
-
-        if (scope.status == 'ready')
-        scope.url = $sce.trustAsResourceUrl('http://fast.wistia.net/embed/iframe/' + scope.hashId);
-        else if (scope.status != 'failed') {
-          $timeout(function(){
-            scope.checkStatus();
-          }, 3000);
-        }
-      });
+  /* @ngInject */
+  function uploadFileDirective($timeout, $sce, $http) {
+    // Usage:
+    //   <uploadfile id="your-element-id" wistiapass="your-wistia-api-password"/>
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'uploadFile.html',
+      replace: true,
+      link: link,
+      scope: {
+        id: "@id",
+        wistiaPassword: "@wistiapass"
+      }
     };
 
-    $timeout(function(){
-      $('#' + scope.id + '_input').fileupload({
-        dataType: 'json',
-        formData: {
-          api_password: scope.wistiaPassword
-        },
-        add: function (e, data) {
-          scope.hashId   = '';
-          scope.progress = 0;
-          scope.status   = 'uploading';
-          scope.url      = '';
+    return directive;
 
-          data.submit();
-        },
-        done: function (e, data) {
-          if (data.result.hashed_id != '') {
-            scope.hashId = data.result.hashed_id;
-            scope.checkStatus();
+    function link(scope, element, attrs) {
+      scope.hashId   = '';
+      scope.progress = 0;
+      scope.status   = 'idle';
+      scope.url      = '';
+
+      scope.checkStatus = function() {
+        $http({
+          method: 'GET',
+          url: 'https://api.wistia.com/v1/medias/' + scope.hashId + '.json?api_password=' + scope.wistiaPassword
+        }).then(function (response) {
+          scope.status = response.data.status || '';
+
+          if (scope.status == 'ready')
+            scope.url = $sce.trustAsResourceUrl('http://fast.wistia.net/embed/iframe/' + scope.hashId);
+          else if (scope.status != 'failed') {
+            //check status again in a few seconds
+            $timeout(function(){
+              scope.checkStatus();
+            }, 3000);
+          }
+        });
+      };
+
+      $timeout(function(){
+        $('#' + scope.id + '_input').fileupload({
+          dataType: 'json',
+          formData: {
+            api_password: scope.wistiaPassword
+          },
+          add: function (e, data) {
+            scope.hashId   = '';
             scope.progress = 0;
-            scope.status = 'UPLOAD A VIDEO'
+            scope.status   = 'uploading';
+            scope.url      = '';
+
+            data.submit();
+          },
+          done: function (e, data) {
+            if (data.result.hashed_id != '') {
+              scope.hashId = data.result.hashed_id;
+              scope.checkStatus();
+            }
+          },
+          progressall: function (e, data) {
+            if (data.total > 0) {
+              scope.$apply(function(){
+                scope.progress = parseInt(data.loaded / data.total * 100, 10);
+              });
+            }
           }
-        },
-        progressall: function (e, data) {
-          if (data.total > 0) {
-            scope.$apply(function(){
-              scope.progress = parseInt(data.loaded / data.total * 100, 10);
-            });
-          }
-        }
+        });
       });
-    });
+    }
   }
-}]);
+})();
